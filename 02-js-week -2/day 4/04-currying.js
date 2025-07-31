7f72682f3cc39b2a7be25fcde6ca168b188aec6c
murshidmhd <murshidmuhammad65@gmail.com>
2025-07-31 15:47:30 +0530

    Add files via upload

---
+++ 02-js-week -2/day 4/04-currying.js
@@ -0,0 +1,69 @@
+// function calculate( a,b,operation){
+
+// }
+
+// function add(){
+//  let result = a + b
+
+// return result
+// }
+
+// function subtract(){
+
+//     return a - b
+// }
+
+// console.log(calculate( 1,2,add))
+
+// function multiply(a, b, callback) {
+//   return callback(a, b);
+// }
+// function doMultiply(a, b) {
+//   return a * b;
+// }
+// function divide(a, b) {
+//   return a / b;
+// }
+// console.log(multiply(2, 3, doMultiply)); // Should log "6"
+
+// q2
+
+// function calculate(a, b, operation) {
+//   return operation(a, b); // Pass a and b to the operation
+// }
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// function divide(a, b) {
+//   return a / b;
+// // }
+
+// console.log(calculate(2, 3, multiply)); // 6
+// console.log(calculate(6, 2, divide)); // 3
+
+
+// function greetUser(){
+
+
+    
+// }
+
+
+
+// function capitalizeWords(str1, letter){
+// let str ='the tiger took the toy'
+// let hi =str.split(' ');
+// // let [1,2,3,4,5] = hi
+
+// for(i = 1; i <= str.length;i++ ){
+// if (i === t )
+// }
+
+// hi.map(nice=> {
+
+//   if( nice.startsWith('t')){
+//     nice[0].toUpperCase
+//   }
+  
